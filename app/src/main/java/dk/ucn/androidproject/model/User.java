58c06eacd9b6278554f926988b41fead8fd6c42f
Mike <mike.breier.olsen@gmail.com>
2015-10-16 11:28:55 +0200

    User model

---
+++ app/src/main/java/dk/ucn/androidproject/model/User.java
@@ -0,0 +1,43 @@
+package dk.ucn.androidproject.model;
+
+/**
+ * Created by Mike on 16-10-2015.
+ */
+public class User {
+    private String name;
+    private String userName;
+    private String password;
+    private String eMail;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+}
