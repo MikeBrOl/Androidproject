36a8ece94b6b64701fa4a281c3a48415e39f8ddc
psyfonde <anna.kelemen.dk>
2015-10-23 14:13:32 +0200

    expandable list view in progress

--- app/src/main/java/dk/ucn/androidproject/activities/EvaluationActivity.java
+++ app/src/main/java/dk/ucn/androidproject/activities/EvaluationActivity.java
@@ -1,19 +1,73 @@
 package dk.ucn.androidproject.activities;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListView;
+
+import java.util.HashMap;
+import java.util.List;
 
 import dk.ucn.androidproject.R;
+import dk.ucn.androidproject.dao.ItemCategoryDao;
+import dk.ucn.androidproject.dao.ItemDescriptionDao;
+import dk.ucn.androidproject.model.ItemCategory;
+import dk.ucn.androidproject.model.ItemDescription;
 
 public class EvaluationActivity extends AppCompatActivity {
+
+    private ItemCategoryDao itemCategoryDao;
+    private ItemDescriptionDao itemDescriptionDao;
+    private ExpandableListView expandableListView;
+    private ExpandableItemListAdapter listAdapter;
+    private List<ItemCategory> listDataHeader;
+    private HashMap<String, List<ItemDescription>> listDataChild;
+
+    private class DownloadDataTask extends AsyncTask<Context, Integer, HashMap<String,List<ItemDescription>>> {
+        @Override
+        protected HashMap<String, List<ItemDescription>> doInBackground(Context... params) {
+            listDataHeader = itemCategoryDao.getAllCategories();
+            return listsToChildData(listDataHeader);
+        }
+
+        private HashMap<String, List<ItemDescription>> listsToChildData(List<ItemCategory> categories) {
+            HashMap<String, List<ItemDescription>> listDataChild = new HashMap<>();
+
+            for (ItemCategory category : categories){
+                List<ItemDescription> descriptions = itemDescriptionDao.getAllByCategory(category.getTitle());
+                listDataChild.put(category.getTitle(), descriptions);
+            }
+            return listDataChild;
+        }
+
+        @Override
+        protected void onPostExecute(HashMap<String, List<ItemDescription>> stringListHashMap) {
+            expandableListView = (ExpandableListView)findViewById(R.id.expandableListView);
+            listAdapter = new ExpandableItemListAdapter(getApplicationContext(), listDataChild, listDataHeader);
+            expandableListView.setAdapter(listAdapter);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluation);
+        Log.i("__Evaluation", "evaluation_activity");
+
+        itemCategoryDao = new ItemCategoryDao(getApplicationContext());
+        itemDescriptionDao = new ItemDescriptionDao(getApplicationContext());
+
+        loadData(getApplicationContext());
     }
+
+    private void loadData(Context applicationContext) {
+        new DownloadDataTask().execute(applicationContext);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/dk/ucn/androidproject/activities/ExpandableItemListAdapter.java
@@ -0,0 +1,102 @@
+package dk.ucn.androidproject.activities;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import dk.ucn.androidproject.R;
+import dk.ucn.androidproject.model.ItemCategory;
+import dk.ucn.androidproject.model.ItemDescription;
+
+/**
+ * Created by ki on 23-10-2015.
+ */
+public class ExpandableItemListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<ItemCategory> listDataHeader;
+    private HashMap<String, List<ItemDescription>> listDataChild;
+
+    public ExpandableItemListAdapter(Context context, HashMap<String, List<ItemDescription>> listDataChild, List<ItemCategory> listDataHeader) {
+        this.context = context;
+        this.listDataChild = listDataChild;
+        this.listDataHeader = listDataHeader;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listDataChild.get(this.listDataHeader.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.listDataChild.get(this.listDataHeader.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String)getGroup(groupPosition).toString();
+        if (convertView == null){
+            LayoutInflater inflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.expandable_list_group, null);
+        }
+        TextView lblListHeader = (TextView)convertView.findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+        return convertView;
+
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String)getChild(groupPosition, childPosition).toString();
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.expandable_list_item, null);
+        }
+
+        TextView txtListChild = (TextView) convertView.findViewById(R.id.lblListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
+
