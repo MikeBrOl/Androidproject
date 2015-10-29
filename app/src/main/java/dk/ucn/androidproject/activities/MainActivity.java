ee8d3b2a8e92c310cbf72595c591c84b07004bda
Mike <mike.breier.olsen@gmail.com>
2015-10-29 10:59:40 +0100

    x

--- app/src/main/java/dk/ucn/androidproject/activities/MainActivity.java
+++ app/src/main/java/dk/ucn/androidproject/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package dk.ucn.androidproject.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import org.jasypt.util.password.BasicPasswordEncryptor;
 
@@ -78,7 +80,12 @@ public class MainActivity extends AppCompatActivity {
         }
         else
         {
-         //bad login!
+         Context context = getApplicationContext();
+            CharSequence text = "Wrong username or password!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 
